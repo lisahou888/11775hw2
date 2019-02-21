91be151721ef1bdca0d1ffb11f464f3e152927c5
Zhuojun Hou <zhuojunh@andrew.cmu.edu>
2019-02-21 14:26:15 -0500

    extracting frames from videos

---
+++ hw2_code/extract_frames.py
@@ -0,0 +1,10 @@
+from ffmpy import FFmpeg
+import os
+
+if not os.path.exists('./frames'):
+        os.mkdir('./frames')
+for video in os.listdir('./downsampled_videos'):
+	name = video[0:video.find('.')]
+	if not os.path.exists('./frames/'+name):
+        os.mkdir('./frames'+name)
+	ff = FFmpeg(inputs={'./downsampled_videos/' + video: None}, outputs={'./frames/'+name+'/'+name+"_%d.png": ['-y', '-vf', 'fps=5']})
