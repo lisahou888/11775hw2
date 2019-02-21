0da322861551fee9a936a3175a111936c8a46135
Ubuntu <ubuntu@ip-172-31-14-52.us-east-2.compute.internal>
2019-02-21 19:36:27 +0000

    working extracting frames

--- hw2_code/extract_frames.py
+++ hw2_code/extract_frames.py
@@ -2,9 +2,13 @@ from ffmpy import FFmpeg
 import os
 
 if not os.path.exists('./frames'):
-        os.mkdir('./frames')
+    os.mkdir('./frames')
 for video in os.listdir('./downsampled_videos'):
-	name = video[0:video.find('.')]
-	if not os.path.exists('./frames/'+name):
-        os.mkdir('./frames'+name)
-	ff = FFmpeg(inputs={'./downsampled_videos/' + video: None}, outputs={'./frames/'+name+'/'+name+"_%d.png": ['-y', '-vf', 'fps=5']})
+    print("********************************")
+    print(video)
+    print("********************************")
+    name = video[0:video.find('.')]
+    if not os.path.exists('./frames/'+name):
+        os.mkdir('./frames/'+name)
+    ff = FFmpeg(inputs={'./downsampled_videos/' + video: None}, outputs={'./frames/'+name+'/'+name+"_%d.png": ['-y', '-vf', 'fps=1/5']})
+    ff.run()
